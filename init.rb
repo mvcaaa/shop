823edf01e455b7e4899c5e58df3157e0d76e7341
Andrey Astashov <mvc.aaa@gmail.com>
2013-04-06 10:13:58 +0700

    email notification support using Pony gem, threads

--- init.rb
+++ init.rb
@@ -10,25 +10,22 @@ StoreApplication.config do |app|
   end
 end
 
-p StoreApplication.environment
-p StoreApplication.name
-p StoreApplication::admin.email
-p StoreApplication::admin.login
-
 @items = []
 @items << RealItem.new({:name=>'car', :price=>400000, :weight=>1001 })
 @items << RealItem.new({:name=>'car', :price=>424000, :weight=>1400 })
 @items << RealItem.new({:name=>'kettle', :price=>20, :weight=>4 })
-@items << VirtualItem.new({:name=>'coupon', :price=>10000})
-@items << AntiqueItem.new({:name=>'ring', :price=>10000})
 
+=begin
 cart = Cart.new('Andrey')
 cart.add_item RealItem.new({:name=>'car', :price=>400000, :weight=>1001 })
 cart.add_item RealItem.new({:name=>'car', :price=>424000, :weight=>1400 })
 cart.add_item RealItem.new({:name=>'kettle', :price=>20, :weight=>4 })
+=end
 
-#method = 'all_cars'
-#p cart.send(method)
+order = Order.new
+@items.each { |i| order.add_item(i) }
+
+order.place
 
 
 
