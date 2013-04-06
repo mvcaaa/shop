e42761c8d3be8b38ad809112b9079e35c617f8d2
Andrey Astashov <mvc.aaa@gmail.com>
2013-04-06 12:28:41 +0700

    Cart pending tests

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -8,12 +8,26 @@ require_relative '../app/cart'
 
 describe Cart do
 
-  it 'Add multiple items into the cart' do
-    cart = Cart.new('Andrey')
-    item1 = Item.new('kettle', price: 200)
-    item2 = Item.new('car', price: 200000)
-    cart.add_items(item1, item2)
+  describe 'managing items' do
 
-    cart.items.should include(item1, item2)
+    it 'Add multiple items into the cart' do
+      cart = Cart.new('Andrey')
+      item1 = Item.new('kettle', price: 200)
+      item2 = Item.new('car', price: 200000)
+      cart.add_items(item1, item2)
+      cart.items.should include(item1, item2)
+    end
+
+    it 'remove items from itself' do
+      pending
+    end
+
   end
+
+
+  it 'cart can count items in itself'
+  it 'cart can checkout order'
+  it 'cart can clean after checkout'
+
+
 end
